4e4cbf349234eae5d2cadabc19bf48db2b255e37
Ranjith Kumar Yaramothu <ryaramot@gitam.in>
2026-09-08 10:03:04 +0530

    main.py

--- Day-22/cal/main.py
+++ Day-22/cal/main.py
@@ -1,6 +1,5 @@
 '''
 import logic
-
 logic.add(20,40)
 logic.sub9(20,40)
 logic.mul(20,40)
@@ -9,7 +8,6 @@ logic.mod(20,40)
 logic.pow(20,40)
 
 import logic as lg
-
 lg.add(20,40)
 lg.sub9(20,40)
 lg.mul(20,40)
@@ -18,7 +16,6 @@ lg.mod(20,40)
 lg.pow(20,40)
 
 from logic import add, mul
-
 add(20,40)
 mul(20,40)
 '''
